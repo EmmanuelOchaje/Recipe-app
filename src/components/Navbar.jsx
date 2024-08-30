228efbd10d1dd1c39f26638394599709db6f72ed
EmmanuelOchaje <114095241+EmmanuelOchaje@users.noreply.github.com>
2024-08-30 03:13:38 +0100

    First-commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+export default function Navbar() {
+  return (
+    <div className="navbar">
+      <a href="#!">
+        T<span>ess</span> Kitchen
+        <div className="nav-links">
+          <a href="#!">Home</a>
+          <a href="#!">Recipe</a>
+          <a href="#!">Settings</a>
+        </div>
+      </a>
+    </div>
+  );
+}
